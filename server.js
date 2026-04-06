f9b6a74871f260b34c8bb9eeb53a0e76b1f6387f
Mugara250 <mbonyumugara52@gmail.com>
2026-04-06 13:53:07 +0200

    setup

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
-const 
+const path = require('path');
+const port = 3000;
+const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  console.log('request served by node.js(express) application');
+});
+
+app.listen(port, () => {
+  console.log(`Node application is listening on port ${port}`);
+});
