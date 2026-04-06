4d71d71f1c6f45c929472b2636f30579e168e418
Mugara250 <mbonyumugara52@gmail.com>
2026-04-06 15:35:15 +0200

    docker compose file

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
+const replicaApp = process.env.APP_NAME;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  console.log('Request served');
+  console.log(`Request served by ${replicaApp}`);
 });
 
 app.listen(port, () => {
-  console.log(`Node application is listening on port ${port}`);
+  console.log(`${replicaApp} is listening on port ${port}`);
 });
